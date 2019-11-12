4e2b0ae3bc869f630e8e235245a651fdb84fefbf
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-11 21:17:42 -0700

    added CRUD functionality

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,48 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+end
